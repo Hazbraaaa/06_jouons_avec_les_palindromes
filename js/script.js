e022a5efcfcc21a0bad2206ec8c20e2acf503805
chazera <hazera.c@gmail.com>
2025-03-12 16:30:07 +0100

    etape 3

--- js/script.js
+++ js/script.js
@@ -8,21 +8,21 @@ function isValidDate(date) {
     const month = splittedDate[1];
     const year = splittedDate[2];
 
-    if (day > maxDayInAMonth(month)) {
+    if (day > maxDayInAMonth(month, year)) {
         return false;
     }
     else if (month > 12) {
         return false;
     }
     else if (year < 999 || year > 9999) {
-        return false;  
+        return false;
     }
     else {
         return true;
     }
 }
 
-function maxDayInAMonth(month) {
+function maxDayInAMonth(month, year) {
     switch (month) {
         case "01":
         case "03":
@@ -31,9 +31,14 @@ function maxDayInAMonth(month) {
         case "08":
         case "10":
         case "12":
-            return "31";   
+            return "31";
         case "02":
-            return "28";
+            if (year % 4 === 0) {
+                return "29";
+            }
+            else {
+                return "28";
+            }
         case "04":
         case "06":
         case "09":
@@ -46,9 +51,9 @@ function isPalindrome(date) {
     if (!isValidDate(date)) {
         return false;
     }
-    
+
     const splittedDate = date.split("/");
-    const dayMonth = splittedDate[0]+splittedDate[1];
+    const dayMonth = splittedDate[0] + splittedDate[1];
     const year = splittedDate[2];
     const monthDay = dayMonth.split("").reverse().join("");
 
@@ -60,4 +65,41 @@ function isPalindrome(date) {
     }
 }
 
-console.log(isPalindrome("20/12/2102"));
+function getCurrentDate() {
+    const currentDate = new Date();
+    let currentDay = currentDate.getDate();
+    let currentMonth = currentDate.getMonth()+1;
+    const currentYear = currentDate.getFullYear();
+
+    if (currentDay < 10) {
+        currentDay = "0"+currentDay;
+    }
+    if (currentMonth < 10) {
+        currentMonth = "0"+currentMonth;
+    }
+    return `${currentDay}/${currentMonth}/${currentYear}`;
+}
+
+function getNextPalindrome(number) {
+    let myDate = getCurrentDate();
+    let myYear = myDate.split("/")[2];
+    let count = 0;
+
+    if (isPalindrome(myDate)) {
+        console.log(myDate);
+    }
+    while (count < number) {
+        myYear += "";
+        let myDay = myYear.split("")[3]+myYear.split("")[2];
+        let myMonth = myYear.split("")[1]+myYear.split("")[0];
+        let dateToCheck = `${myDay}/${myMonth}/${myYear}`;
+        
+        if (isPalindrome(dateToCheck)) {
+            console.log(dateToCheck);
+            count++;
+        }
+        myYear++;
+    }
+}
+
+getNextPalindrome(10);
